7181325bf9674b6430dc1820ddf7efda226573aa
Martin Skrim <64171979+martin-skrim@users.noreply.github.com>
2026-04-27 23:38:03 +0200

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,6 @@
+self.addEventListener('install', (e) => {
+    console.log('[Service Worker] Install');
+});
+self.addEventListener('fetch', (e) => {
+    // Оставляем пустым, он нужен просто чтобы браузер разрешил установку
+});
